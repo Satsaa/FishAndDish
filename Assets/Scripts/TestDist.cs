2e29949d9b324fb2dc088e15c6cf35b500e08ad9
Samuel <32210043+Satsaa@users.noreply.github.com>
2020-01-26 20:14:31 +0200

    Stuffies (actually nothing project related)

---
+++ Assets/Scripts/TestDist.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TestDist : MonoBehaviour {
+  public Transform a1;
+  public Transform a2;
+  public Transform b1;
+  public Transform b2;
+
+  // Update is called once per frame
+  void Update() {
+    Line a = new Line(a1.position, a2.position);
+    Line b = new Line(b1.position, b2.position);
+    a.Debug(Color.red);
+    b.Debug(Color.green);
+
+    var res = a.DistanceLine(b);
+    res.Debug(Color.blue);
+  }
+}
