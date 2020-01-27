c811abbed7b6a4bb70a24a4dc83b3e0b0c1c14d1
Samuel Förström <samppa.f@gmail.com>
2020-01-27 16:03:17 +0200

    asdasdad

---
+++ Assets/MyUnityCollection/Scripts/Types/Geometric/Box.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class Box {
+  public float2 center;
+  public float2 size;
+
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  void OnDrawGizmosSelected() {
+    Gizmos.color = Color.magenta;
+    Gizmos.DrawWireCube(center, size);
+
+    Gizmos.color = Color.magenta * 0.2f;
+    Gizmos.DrawCube(center, size);
+  }
+}
